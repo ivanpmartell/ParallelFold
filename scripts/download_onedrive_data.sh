cb0f97bc095b4ebd01bc61b51a193fcc470d245a
Ivan Perez <ivan.pmartell@hotmail.com>
2024-01-19 13:25:03 +0900

    Easier download of af2 database files (avoid broken links)

---
+++ scripts/download_onedrive_data.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+#Must have lua5.3 installed
+SCRIPT_DIR=$( cd -- "$( dirname -- "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )
+DOWNLOAD_DIR=$(realpath "$1")
+
+cd $DOWNLOAD_DIR
+git clone https://gitlab.com/VADemon/onedeath.git
+cd onedeath
+lua main.lua 'https://1drv.ms/f/s!AjSouo0_uGDBhMJ_x2uiiHxS08T4jQ'
+mv Navi-20_af2_databases/* $DOWNLOAD_DIR
+cd $DOWNLOAD_DIR
+rm -rf onedeath
+
+cd $SCRIPT_DIR
+./download_pdb_mmcif.sh $DOWNLOAD_DIR
